87866bbc8a2ed8415995acd41e7e3d397762f5f8
chloekelly <61448061+chloekelly@users.noreply.github.com>
2020-04-25 19:42:42 +1000

    Add files via upload

---
+++ Assets 2/scripts/Webcam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+
+    int currentCamIndex = 0;
+
+    WebCamTexture tex;
+
+    public RawImage display; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tex != null)
+        {
+            display.texture = null;
+            tex.Stop();
+            tex = null;
+        }
+        else
+        {
+            WebCamDevice device = WebCamTexture.devices[currentCamIndex];
+            tex = new WebCamTexture(device.name);
+            display.texture = tex;
+
+            tex.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
